cc08059e417fdf0c6d95457251d082b6f19e81c4
Fawad Iqbal <fwd82@live.com>
2019-04-08 00:05:49 +0500

    Fixing class of btn

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -4,7 +4,7 @@
     <?php
     
         echo "<br><h2> DASHBOARD </h2>"; 
-        echo "<p> Welcome to Dashboard </p>"; 
+        echo "<p> Welcome to Dashboard, Here will show all the components </p>"; 
         echo anchor('http://localhost/proverbona1/dashboard/insert_proverb', 'Add Article', 'title="Add Article"');
     ?>
     <br>
@@ -17,7 +17,7 @@
     </div>
     <div class="col-lg-6">
         <a class="btn btn-secondary" title="User Profile" href="http://localhost/proverbona1/dashboard/user_profile">User Profile</a>
-        <a class="btn btn-success" title="Link two Proverbs" href="http://localhost/proverbona1/dashboard/link">Link two Proverbs</a>
+        <a class="btn btn-secondary" title="Link two Proverbs" href="http://localhost/proverbona1/dashboard/link">Link two Proverbs</a>
     
     </div>
 </div>
