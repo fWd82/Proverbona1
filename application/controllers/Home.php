87b0d46ea08353105036558c9fb44f7f6c328104
Fawad Iqbal <fwd82@live.com>
2019-04-10 16:36:50 +0500

    ~

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,12 @@
 
             $all_proverbs = $this->Proverbs_model->proverb_list();
             $this->load->view('public/home', ['all_proverb1'=>$all_proverbs]);
-
+        }
+        public function proverb($id){
+            $this->load->model('Proverbs_model');
+            $my_proverbs = $this->Proverbs_model->get_proverb_id($id);
+            $this->load->view('public/proverb_detail', compact('my_proverbs'));
         }
     }
+
 ?>
