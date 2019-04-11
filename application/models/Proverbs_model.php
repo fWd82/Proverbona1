23dad67509be4de753259363d3db8437ef4f7838
Fawad Iqbal <fwd82@live.com>
2019-04-11 18:18:22 +0500

    Querying JOIN

--- application/models/Proverbs_model.php
+++ application/models/Proverbs_model.php
@@ -1,39 +1,30 @@
 <?php
     class Proverbs_model extends CI_Model{
-        public function index(){
-            // View all proverbs here
-        } // eof index()
-
         public function proverb_list(){
+
+            // SELECT proverb_id, proverb_statement, proverb_tags, table_user.user_name, proverb_timestamp
+            // FROM `table_proverb` 
+            // INNER JOIN table_user ON 
+            //      table_proverb.proverb_addedby = table_user.user_id;
+            
             $this->load->database();
             $query = $this->db
-                                ->select(['proverb_id','proverb_statement', 'proverb_tags', 'proverb_timestamp'])
+                                ->select(['proverb_id','proverb_statement', 'proverb_tags', 'table_user.user_name', 'proverb_timestamp'])
                                 ->from('table_proverb')
                                 ->order_by('proverb_timestamp', 'DESC')
+                                ->join('table_user', 'table_proverb.proverb_addedby = table_user.user_id')
                                 ->get();
             return $query->result();
-
-
         } // eof proverb_list()
 
 
         public function add_proverb($array){
             $this->load->database();
             return $this->db->insert('table_proverb', $array);
-
-            
-            // print_r($post); exit();
-            // echo "<pre>";
-            // print_r($array); 
-            // exit();
-            
         } // eof add_proverb()
-        // public function edit_proverb(){}
-        // public function delete_proverb(){}
-
+        
         public function get_reference() {
             $result = $this -> db -> select('reference_id, reference_category, reference_title, reference_author') -> get('table_reference') -> result_array(); 
-         
             $proverb_reference = array(); 
             foreach($result as $r) { 
                 $proverb_reference[$r['reference_id']] = $r['reference_category']  . ": " . $r['reference_title'] . " - " . $r['reference_author']; 
@@ -42,7 +33,7 @@
             // echo "<pre>";
             // print_r ($proverb_reference); exit;
             return $proverb_reference; 
-            } // eof get_reference()
+        } // eof get_reference()
 
         public function get_proverb_id($id) {
             $q = $this->db->from('table_proverb')
@@ -51,8 +42,16 @@
             if ($q->num_rows()) 
                 return $q->row();
             return false;
-        }
+        } // eof get_proverb_id()
+    
     }
+
+    // public function edit_proverb(){}
+    // public function delete_proverb(){}
+    // print_r($post); exit();
+    // echo "<pre>";
+    // print_r($array); 
+    // exit();
 
 
 
